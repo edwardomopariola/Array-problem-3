bae1b9400b8bde773030d36fcb0cc3219aec7a5c
Edward Omopariola <femiedjr@gmail.com>
2024-08-01 18:28:45 -0400

    adjustment on js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const nums = [5, 8, 4, -6, 34, 12, -22, 5, 35];
 
 function sortNumsInt(array) { // i define my function with an array in the parameter
-    return nums.sort(function(a,b){ //Using the .sort method, i can compare the two values a and b
+    return array.sort(function(a,b){ //Using the .sort method, i can compare the two values a and b
         return b - a; // to sorrt this in descendinng order, we subtract the second parameter from the first parameter
     });
 }
